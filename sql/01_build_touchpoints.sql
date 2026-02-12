133904f9ca130600effa6669a17a8ce991dfb32b
Anastasia Sipatina <141122219+Anastasia-Sipatina@users.noreply.github.com>
2026-02-12 20:31:38 +0300

    Update 01_build_touchpoints.sql

--- sql/01_build_touchpoints.sql
+++ sql/01_build_touchpoints.sql
@@ -1,16 +1,20 @@
+-- Build a touchpoints table for attribution models (BigQuery SQL)
+
 CREATE OR REPLACE TABLE `final_dataset.touchpoints` AS
-WITH unified AS (
-  SELECT
-    user_id,
-    event_dt,
-    channel,
-    campaign,
-    interaction_type,
-    SAFE_CAST(conversions AS INT64) AS conversions,
-    SAFE_CAST(revenue AS FLOAT64) AS revenue
-  FROM `raw_dataset.source_events`
-)
-SELECT *
-FROM unified
-WHERE user_id IS NOT NULL
+SELECT
+  clientID AS user_id,
+  DATETIME(datetime) AS event_dt,
+
+  IFNULL(Channel,   'None') AS channel,
+  IFNULL(Publisher, 'None') AS publisher,
+  IFNULL(Campaign,  'None') AS campaign,
+  IFNULL(Creative,  'None') AS creative,
+
+  SAFE_CAST(impressions AS INT64) AS impressions,
+  SAFE_CAST(visits AS INT64) AS clicks,
+  SAFE_CAST(conversions AS INT64) AS conversions,
+  IFNULL(Path_type, 'Primary') AS path_type
+
+FROM `raw_dataset.df_metrics_and_attr`
+WHERE clientID IS NOT NULL
 ORDER BY user_id, event_dt;
